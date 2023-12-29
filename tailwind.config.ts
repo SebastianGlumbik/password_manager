1478a2a29d8a18acd080dedfb7df77dc303bc536
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2023-12-29 11:42:50 +0100

    Project configs

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{html,css,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  //darkMode: 'class'
+} satisfies Config

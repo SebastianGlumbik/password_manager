136c188fb725466f563901e055d2a83cc9fb19cc
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-01-14 00:30:08 +0100

    start over moved to window menu

--- src-tauri/src/frontend/login.rs
+++ src-tauri/src/frontend/login.rs
@@ -4,42 +4,53 @@ use super::*;
 /// - Checks if the database exist
 /// - Opens the database
 #[tauri::command]
-pub fn login(
-    password: &str,
-    connection: State<DatabaseConnection>,
-    app_handle: tauri::AppHandle,
-    window: tauri::Window,
+pub async fn login<'a, 'b>(
+    password: &'a str,
+    connection: State<'b, DatabaseConnection>,
+    app_handle: AppHandle,
+    window: Window,
 ) -> Result<(), &'static str> {
     if database_exists(app_handle.clone()).is_none() {
-        return Err("Database does not exist, please start over");
+        tauri::api::dialog::blocking::message(
+            Some(&window),
+            "Error",
+            "Database does not exist, application will now restart",
+        );
+        app_handle.restart();
     }
 
     connect_database(password, connection, app_handle.clone())?;
 
-    window.close().map_err(|_| "Failed to close window")?;
-
-    // TODO change also in register.rs maybe separate function for building window
-    WindowBuilder::new(
-        &app_handle,
-        "main",
-        tauri::WindowUrl::App("/src/main.html".into()),
-    )
-    .resizable(true)
-    .title("Password Manager")
-    .min_inner_size(640f64, 480f64)
-    .inner_size(800f64, 600f64)
-    .build()
-    .map_err(|_| "Failed to open new window")?;
+    create_main_window(app_handle, window)?;
 
     Ok(())
 }
 
 /// Deletes the database file and restarts the application.
-#[tauri::command]
-pub fn start_over(app_handle: tauri::AppHandle) -> Result<(), &'static str> {
+/// - Has a confirmation dialog before deleting the database file
+/// - Has a message dialog if the database file could not be deleted
+pub fn start_over(app_handle: AppHandle, window: Window) {
     if let Some(path_buf) = database_exists(app_handle.clone()) {
-        fs::remove_file(path_buf).map_err(|_| "Failed to delete database")?;
+        let window_close = window.clone();
+        tauri::api::dialog::ask(
+            Some(&window),
+            "Starting over",
+            "Are you sure you want to continue? This action will permanently delete all passwords.",
+            move |answer| {
+                if answer {
+                    if let Err(error) = fs::remove_file(path_buf) {
+                        tauri::api::dialog::message(
+                            Some(&window_close),
+                            "Error",
+                            format!("Failed to delete database file: {}", error),
+                        );
+                    } else {
+                        app_handle.restart();
+                    }
+                }
+            },
+        );
+    } else {
+        app_handle.restart();
     }
-    app_handle.restart();
-    Ok(())
 }

709de02b7200b37f9058991f0a050cc641757206
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-01-21 15:01:44 +0100

    Added loading indicators and commented the code

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,11 @@
-import {createResource} from "solid-js";
+import {createResource, JSX} from "solid-js";
 import {invoke} from "@tauri-apps/api/tauri";
 
-export default function main() {
+/**
+ * Main page. This is where all the magic happens.
+ * @return {JSX.Element} - Div containing the main page.
+ */
+export default function main(): JSX.Element {
     const [data] = createResource(async () => invoke("load_data"));
     return (
         <div class="h-full flex flex-col items-center justify-center">
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {createResource, Suspense, Switch, Match} from "solid-js";
+import {createResource, Suspense, Switch, Match, JSX} from "solid-js";
 import {render} from "solid-js/web";
 import { invoke } from "@tauri-apps/api/tauri";
 import Login from './Login.tsx';
@@ -7,13 +7,27 @@ import Main from './Main.tsx';
 import Loading from "./Loading.tsx";
 import "./style.css"
 
-function App() {
+/*
+//TODO: Add in final release
+window.addEventListener('contextmenu', function(event) {
+    let target = event.target as HTMLInputElement;
+    if(target.type !== "password" && target.type !== "text") {
+        event.preventDefault();
+    }
+});
+*/
+
+/**
+ * Main App component. Based on the window state, it renders the correct component.
+ * @return {JSX.Element} - Div containing the main app.
+ */
+function App(): JSX.Element {
     const [window] = createResource(async () => invoke("initialize_window"));
 
     return (
         <Suspense fallback={<Loading/>}>
-            <Switch fallback={<div>Error</div>}>
-                <Match when={window() === "Register"}>
+            <Switch fallback={<div>Not Found</div>}>
+            <Match when={window() === "Register"}>
                     <Register />
                 </Match>
                 <Match when={window() === "Login"}>
@@ -27,4 +41,7 @@ function App() {
     );
 }
 
+/**
+ * Renders the app.
+ */
 render(App, document.getElementById('root') as HTMLElement);

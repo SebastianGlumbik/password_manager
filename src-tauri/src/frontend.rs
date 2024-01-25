89ea7295e66be54d088137c2b49fcdaf22192f37
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-01-25 23:37:57 +0100

    Added methods to DatabaseConnection, functions moved to separate files

--- src-tauri/src/frontend.rs
+++ src-tauri/src/frontend.rs
@@ -1,144 +1,95 @@
-mod login;
-mod main;
-mod register;
+mod command;
+mod event;
+mod menu;
+mod window;
+
+pub use command::*;
+pub use event::*;
+pub use menu::*;
+pub use window::*;
 
 use super::*;
-pub use login::*;
-pub use main::*;
-pub use register::*;
 use secrecy::{ExposeSecret, SecretString};
 use std::fs;
+use std::ops::Not;
 use std::path::PathBuf;
 use std::sync::Mutex;
-#[cfg(target_os = "linux")]
-use tauri::Submenu;
-use tauri::{AppHandle, CustomMenuItem, Menu, MenuEntry, MenuItem, State, Window, WindowBuilder};
+use tauri::{AppHandle, State, Window};
 
 /// Name of the database file.
-const DATABASE_FILE_NAME: &str = "database.db";
+pub const DATABASE_FILE_NAME: &str = "database.password_manager";
 
 /// Database connection for tauri state.
 #[derive(Default)]
 pub struct DatabaseConnection {
-    database: Mutex<Option<Database>>,
+    database_mutex: Mutex<Option<Database>>,
 }
 
-/// Looks for a database file in the app local data directory and returns its path if it exists.
-/// Path for data directory:
-/// - macOS: ~/Library/Application Support/\<APPLICATION\>
-/// - Linux:  ~/.local/share/\<APPLICATION\>
-#[tauri::command]
-pub async fn database_exists(app_handle: AppHandle) -> Option<PathBuf> {
-    if let Some(path) = app_handle
-        .path_resolver()
-        .app_local_data_dir()
-        .map(|path_buf| path_buf.join(DATABASE_FILE_NAME))
-    {
-        if path.exists() {
-            return Some(path);
+impl DatabaseConnection {
+    /// Opens connection to the database. If the database does not exist, it will be created.
+    /// # Errors
+    /// Returns an error if the database cannot be opened.
+    pub fn connect(
+        &self,
+        password: SecretString,
+        app_handle: AppHandle,
+    ) -> Result<(), &'static str> {
+        let path = Self::database_path(app_handle.clone()).ok_or("Failed to get database path")?;
+
+        if path.exists().not() {
+            fs::create_dir_all(path.parent().ok_or("Failed to get data directory path")?)
+                .map_err(|_| "Failed to create data directory")?;
+        }
+
+        let path = path.to_str().ok_or("Path is not valid UTF-8")?;
+
+        match self.database_mutex.lock() {
+            Ok(mut guard) => {
+                *guard = Some(Database::open(path, password.expose_secret())?);
+                Ok(())
+            }
+            Err(_) => Err("Failed to access database lock"),
         }
     }
 
-    None
-}
-
-/// Window types that can be created.
-#[derive(Clone, serde::Serialize)]
-pub enum WindowType {
-    Login,
-    Register,
-    Main,
-}
-
-/// Creates specific window based on the database state and returns the window type.
-/// # Errors
-/// Returns an error if the window cannot be created.
-#[tauri::command]
-pub async fn initialize_window<'a>(
-    connection: State<'a, DatabaseConnection>,
-    app_handle: AppHandle,
-) -> tauri::Result<WindowType> {
-    if let Ok(database) = connection.database.lock() {
-        if database.is_some() {
-            create_main_window(app_handle)?;
-            return Ok(WindowType::Main);
+    /// Closes connection to the database.
+    /// # Errors
+    /// Returns an error if the database cannot be closed.
+    pub fn disconnect(&self) -> Result<(), &'static str> {
+        match self.database_mutex.lock() {
+            Ok(mut guard) => {
+                *guard = None;
+                Ok(())
+            }
+            Err(_) => Err("Failed to access database lock"),
         }
     }
 
-    match database_exists(app_handle.clone()).await {
-        Some(_) => {
-            create_login_window(app_handle)?;
-            Ok(WindowType::Login)
-        }
-        None => {
-            create_register_window(app_handle)?;
-            Ok(WindowType::Register)
-        }
+    /// Checks if the database is connected. Returns false if the database is not connected or if the database mutex is poisoned.
+    pub fn is_connected(&self) -> bool {
+        self.database_mutex
+            .lock()
+            .map(|guard| guard.is_some())
+            .unwrap_or(false)
     }
-}
 
-/// Opens connection to the database. If the database does not exist, it will be created.
-/// # Errors
-/// Returns an error if the database cannot be opened.
-pub async fn connect_database<'a, 'b>(
-    password: SecretString,
-    connection: State<'b, DatabaseConnection>,
-    app_handle: AppHandle,
-) -> Result<(), &'static str> {
-    let path = match database_exists(app_handle.clone()).await {
-        Some(path_buf) => path_buf
-            .to_str()
-            .ok_or("Path is not valid UTF-8")?
-            .to_string(),
-        None => {
-            let path_buf = app_handle
-                .path_resolver()
-                .app_local_data_dir()
-                .ok_or("Failed to get data directory path")?;
-            fs::create_dir_all(&path_buf).map_err(|_| "Failed to create data directory")?;
-            path_buf
-                .join(DATABASE_FILE_NAME)
-                .to_str()
-                .ok_or("Path is not valid UTF-8")?
-                .to_string()
-        }
-    };
-
-    match connection.database.lock() {
-        Ok(mut database) => {
-            *database = Some(Database::open(&path, password.expose_secret())?);
-            Ok(())
-        }
-        Err(_) => Err("Failed to access database lock"),
+    /// Returns full path to the database file based on the app local data directory.
+    /// Paths:
+    /// - macOS: ~/Library/Application Support/\<APPLICATION\>/[`DATABASE_FILE_NAME`]
+    /// - Linux:  ~/.local/share/\<APPLICATION\>/[`DATABASE_FILE_NAME`]
+    pub fn database_path(app_handle: AppHandle) -> Option<PathBuf> {
+        app_handle
+            .path_resolver()
+            .app_local_data_dir()
+            .map(|path_buf| path_buf.join(DATABASE_FILE_NAME))
     }
-}
 
-/// Deletes the database file and restarts the application.
-/// # Dialogs
-/// - Has a confirmation dialog before deleting the database file
-/// - Has a message dialog if the database file could not be deleted
-pub async fn start_over(app_handle: AppHandle, window: Window) {
-    if let Some(path_buf) = database_exists(app_handle.clone()).await {
-        let window_close = window.clone();
-        tauri::api::dialog::ask(
-            Some(&window),
-            "Starting over",
-            "Are you sure you want to continue? This action will permanently delete all passwords.",
-            move |answer| {
-                if answer {
-                    if let Err(error) = fs::remove_file(path_buf) {
-                        tauri::api::dialog::message(
-                            Some(&window_close),
-                            "Error",
-                            format!("Failed to delete database file: {}", error),
-                        );
-                    } else {
-                        app_handle.restart();
-                    }
-                }
-            },
-        );
-    } else {
-        app_handle.restart();
+    /// Checks if the database file exists.
+    pub fn database_exists(app_handle: AppHandle) -> bool {
+        if let Some(path) = Self::database_path(app_handle) {
+            return path.exists();
+        }
+
+        false
     }
 }

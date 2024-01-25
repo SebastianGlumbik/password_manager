b7550a392c4dd134d261ab72d74210a8be1506e7
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-01-25 23:33:47 +0100

    Cosmetic changes

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -2,14 +2,14 @@ import {JSX} from "solid-js";
 
 /**
  * Loading component
- * Has a loading spinner and a text saying "Loading, please wait"
+ * Has a loading spinner and a text saying "Loading...", aligned in the center of the screen.
  * @return {JSX.Element} - Div containing the loading component.
  */
 export default function Loading(): JSX.Element {
   return (
-      <div class="h-full flex flex-col items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-10 animate-spin" ><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
-          <p class="text-2xl">Loading, please wait</p>
+      <div class="absolute bottom-0 h-full flex flex-col items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-7 animate-spin" ><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
+          <p class="text-lg">Loading...</p>
       </div>
   );
 }
--- src/Register.tsx
+++ src/Register.tsx
@@ -37,7 +37,7 @@ export default function Register(): JSX.Element {
             }}>
                 <div class="relative w-full my-3">
                     <input placeholder="Enter your master password" type={visibility() ? 'text' : 'password'} class="pl-4 w-full h-7 rounded-xl pr-10" onChange={(event) => setInput(event.currentTarget.value)}></input>
-                        <div class="absolute inset-y-0 right-3 flex items-center" onClick={() => setVisibility(!visibility())}>
+                        <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer" onClick={() => setVisibility(!visibility())}>
                             <Show when={visibility()} fallback={
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-full p-1"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
                             }>
@@ -47,7 +47,7 @@ export default function Register(): JSX.Element {
                 </div>
                 <div class="relative w-full mt-3">
                     <input placeholder="Confirm your master password" type={confirmVisibility() ? 'text' : 'password'} class="pl-4 w-full h-7 rounded-xl pr-10" onChange={(event) => setConfirmInput(event.currentTarget.value)}></input>
-                        <div class="absolute inset-y-0 right-3 flex items-center" onClick={() => setConfirmVisibility(!confirmVisibility())}>
+                        <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer" onClick={() => setConfirmVisibility(!confirmVisibility())}>
                             <Show when={confirmVisibility()} fallback={
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-full p-1"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
                             }>

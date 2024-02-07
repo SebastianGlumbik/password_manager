c3bb99eddb03d2f9627b8ee92241a80835c1bea8
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-07 20:16:33 +0100

    Refactoring

--- src-tauri/src/frontend.rs
+++ src-tauri/src/frontend.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 pub use window::*;
 
 use super::*;
+use crate::database::Database;
 use secrecy::{ExposeSecret, SecretString};
 use std::fs;
 use std::ops::Not;
@@ -21,22 +22,22 @@ use totp_rs::{Rfc6238, TOTP};
 /// Name of the database file.
 pub const DATABASE_FILE_NAME: &str = "database.password_manager";
 
-/// Database connection for tauri state.
+/// Database connection for tauri state. Used for accessing the database.
 #[derive(Default)]
 pub struct DatabaseConnection {
-    database_mutex: Mutex<Option<Database>>,
+    database: Mutex<Option<Database>>,
 }
 
 impl DatabaseConnection {
     /// Opens connection to the database. If the database does not exist, it will be created.
-    /// # Errors
+    /// # Error
     /// Returns an error if the database cannot be opened.
     pub fn connect(
         &self,
         password: SecretString,
         app_handle: AppHandle,
     ) -> Result<(), &'static str> {
-        let path = Self::database_path(app_handle.clone()).ok_or("Failed to get database path")?;
+        let path = Self::database_path(app_handle).ok_or("Failed to get database path")?;
 
         if path.exists().not() {
             fs::create_dir_all(path.parent().ok_or("Failed to get data directory path")?)
@@ -45,31 +46,29 @@ impl DatabaseConnection {
 
         let path = path.to_str().ok_or("Path is not valid UTF-8")?;
 
-        match self.database_mutex.lock() {
-            Ok(mut guard) => {
-                *guard = Some(Database::open(path, password.expose_secret())?);
-                Ok(())
-            }
-            Err(_) => Err("Failed to access database lock"),
-        }
+        let mut guard = self
+            .database
+            .lock()
+            .map_err(|_| "Failed to access database lock")?;
+        *guard = Some(Database::open(path, password.expose_secret())?);
+        Ok(())
     }
 
     /// Closes connection to the database.
-    /// # Errors
+    /// # Error
     /// Returns an error if the database cannot be closed.
     pub fn disconnect(&self) -> Result<(), &'static str> {
-        match self.database_mutex.lock() {
-            Ok(mut guard) => {
-                *guard = None;
-                Ok(())
-            }
-            Err(_) => Err("Failed to access database lock"),
-        }
+        let mut guard = self
+            .database
+            .lock()
+            .map_err(|_| "Failed to access database lock")?;
+        *guard = None;
+        Ok(())
     }
 
     /// Checks if the database is connected. Returns false if the database is not connected or if the database mutex is poisoned.
     pub fn is_connected(&self) -> bool {
-        self.database_mutex
+        self.database
             .lock()
             .map(|guard| guard.is_some())
             .unwrap_or(false)
@@ -96,26 +95,16 @@ impl DatabaseConnection {
     }
 }
 
+/// TOTP manager for tauri state. Used for managing TOTP secrets and generating codes.
 #[derive(Default)]
 pub struct TOTPManager {
-    totp_map: Mutex<HashMap<u64, TOTP>>,
+    hash_map: Mutex<HashMap<u64, TOTP>>,
 }
 
 impl TOTPManager {
-    pub fn add_url(&self, id: u64, url: SecretString) -> Result<(), &'static str> {
-        let Ok(totp) = TOTP::from_url(url.expose_secret()) else {
-            return Err("Invalid OTP Auth URL");
-        };
-
-        match self.totp_map.lock() {
-            Ok(mut guard) => {
-                guard.insert(id, totp);
-                Ok(())
-            }
-            Err(_) => Err("Failed to access manager lock"),
-        }
-    }
-
+    /// Adds a new TOTP secret to the manager.
+    /// # Error
+    /// Returns an error if the secret is invalid or if the manager mutex is poisoned.
     pub fn add_secret(&self, id: u64, secret: String) -> Result<(), &'static str> {
         let Ok(secret) = totp_rs::Secret::Encoded(secret).to_bytes() else {
             return Err("Invalid OTP Secret");
@@ -127,29 +116,28 @@ impl TOTPManager {
             return Err("Invalid OTP Secret");
         };
 
-        match self.totp_map.lock() {
-            Ok(mut guard) => {
-                guard.insert(id, totp);
-                Ok(())
-            }
-            Err(_) => Err("Failed to access manager lock"),
-        }
+        let mut guard = self
+            .hash_map
+            .lock()
+            .map_err(|_| "Failed to access manager lock")?;
+        guard.insert(id, totp);
+        Ok(())
     }
 
+    /// Generates a TOTP code for the given secret.
+    /// # Return
+    /// Returns the current TOTP code and the time to live in seconds or None if the secret does not exist or if the manager mutex is poisoned.
     pub fn get_code(&self, id: &u64) -> Option<(String, u64)> {
-        if let Ok(mut guard) = self.totp_map.lock() {
-            if let Some(totp) = guard.get_mut(id) {
-                let current = totp.generate_current().ok()?;
-                let ttl = totp.ttl().ok()?;
-                return Some((current, ttl));
-            }
-        }
-
-        None
+        let mut guard = self.hash_map.lock().ok()?;
+        let totp = guard.get_mut(id)?;
+        let current = totp.generate_current().ok()?;
+        let ttl = totp.ttl().ok()?;
+        Some((current, ttl))
     }
 
+    /// Removes a TOTP secret from the manager.
     pub fn remove(&self, id: &u64) {
-        self.totp_map.lock().ok().map(|mut guard| {
+        self.hash_map.lock().ok().map(|mut guard| {
             guard.remove(id);
         });
     }

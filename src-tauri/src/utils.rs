28e87502526564e511db78effe1327542a9f0f0c
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-07 22:32:50 +0100

    Refactored

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,5 +1,6 @@
 pub mod password {
     use secrecy::{ExposeSecret, SecretString};
+    use sha1::digest::generic_array::functional::FunctionalSequence;
     use sha1::{Digest, Sha1};
 
     pub async fn is_exposed<S: AsRef<str>>(
@@ -7,14 +8,13 @@ pub mod password {
     ) -> Result<bool, Box<dyn std::error::Error>> {
         let mut hasher = Sha1::new();
         hasher.update(password.as_ref().as_bytes());
-        let hash: SecretString = SecretString::new(
-            hasher
-                .finalize()
-                .iter()
-                .map(|byte| format!("{:02x}", byte))
-                .collect::<String>()
-                .to_uppercase(),
-        );
+        let hash: SecretString = SecretString::new(hasher.finalize().fold(
+            String::with_capacity(40),
+            |mut acc, byte| {
+                acc.push_str(&format!("{:02x}", byte).to_uppercase());
+                acc
+            },
+        ));
         let (prefix, suffix) = hash.expose_secret().split_at(5);
         let url = SecretString::new(format!("https://api.pwnedpasswords.com/range/{}", prefix));
         let response = SecretString::new(reqwest::get(url.expose_secret()).await?.text().await?);

68f6862c93a2eeb4ebf2c819e13bb9181ae37670
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-07 20:15:00 +0100

    Deleted validation

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,9 +1,7 @@
 pub mod password {
-    pub use passwords::analyzer::*;
-    pub use passwords::scorer::*;
-    pub use passwords::*;
     use secrecy::{ExposeSecret, SecretString};
     use sha1::{Digest, Sha1};
+
     pub async fn is_exposed<S: AsRef<str>>(
         password: S,
     ) -> Result<bool, Box<dyn std::error::Error>> {
@@ -20,18 +18,11 @@ pub mod password {
         let (prefix, suffix) = hash.expose_secret().split_at(5);
         let url = SecretString::new(format!("https://api.pwnedpasswords.com/range/{}", prefix));
         let response = SecretString::new(reqwest::get(url.expose_secret()).await?.text().await?);
-        let result = response
+        Ok(response
             .expose_secret()
             .lines()
-            .any(|line| line.starts_with(suffix));
-        Ok(result)
+            .any(|line| line.starts_with(suffix)))
     }
-}
-
-pub mod validate {
-    pub use card_validate::Type as card_type;
-    pub use card_validate::Validate as card;
-    pub use card_validate::ValidateError as card_error;
 }
 
 //TODO Tests

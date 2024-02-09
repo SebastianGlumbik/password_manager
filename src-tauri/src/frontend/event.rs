0fb4e7309321a705c123df1b78d28c7dffd1d486
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-02-09 15:41:31 +0100

    Database export

--- src-tauri/src/frontend/event.rs
+++ src-tauri/src/frontend/event.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::database::model::{Category, Record};
+use std::fmt::format;
 use tauri::MenuEvent;
 
 /// Shows a critical error message and restarts the application.
@@ -21,31 +22,37 @@ pub fn menu_event(event: MenuEvent, app_handle: AppHandle, window: Window) {
     let thread = thread_builder.spawn(move || match event.menu_item_id() {
         "Start Over" => block_on(start_over(app_handle, window_clone)),
         "Choose database" => block_on(choose_database(app_handle, window_clone)),
+        "Settings" => window_clone.emit("settings", ()).unwrap_or_default(),
         "New Login" => window_clone
             .emit_all(
                 "new_record",
                 Record::new("".to_string(), "".to_string(), Category::Login),
             )
-            .unwrap(),
+            .unwrap_or_default(),
         "New Bank Card" => window_clone
             .emit(
                 "new_record",
                 Record::new("".to_string(), "".to_string(), Category::BankCard),
             )
-            .unwrap(),
+            .unwrap_or_default(),
         "New Note" => window_clone
             .emit(
                 "new_record",
                 Record::new("".to_string(), "".to_string(), Category::Note),
             )
-            .unwrap(),
+            .unwrap_or_default(),
         "New Other" => window_clone
             .emit(
                 "new_record",
                 Record::new("".to_string(), "".to_string(), Category::Other),
             )
-            .unwrap(),
-        _ => println!("Unknown menu item: {}", event.menu_item_id()),
+            .unwrap_or_default(),
+        "Export Database" => block_on(export_database(app_handle, window_clone)),
+        _ => tauri::api::dialog::message(
+            Some(&window_clone),
+            "Error",
+            format!("Unknown menu item: {}", event.menu_item_id()),
+        ),
     });
 
     if thread.is_err() {
@@ -107,6 +114,26 @@ pub async fn choose_database(app_handle: AppHandle, window: Window) {
                     );
                 }
                 app_handle.restart();
+            }
+        }
+    }
+}
+
+/// Exports the database file. Has blocking dialogs, so it cannot be run on the main thread.
+pub async fn export_database(app_handle: AppHandle, window: Window) {
+    if let Some(source) = database_path(app_handle.clone()) {
+        if let Some(destination) = tauri::api::dialog::blocking::FileDialogBuilder::new()
+            .set_parent(&window)
+            .set_title("Export database")
+            .set_file_name(DATABASE_FILE_NAME)
+            .save_file()
+        {
+            if let Err(error) = fs::copy(source, destination) {
+                tauri::api::dialog::blocking::message(
+                    Some(&window),
+                    "Error",
+                    format!("Failed to copy database file: {}", error),
+                );
             }
         }
     }

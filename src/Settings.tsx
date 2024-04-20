4114744ea8ef3db98faa3e8ceb2595d31c658784
Sebastian Glumbík <sebastian.glumbik@icloud.com>
2024-04-20 17:32:02 +0200

    Code refactor

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -3,7 +3,7 @@ import {invoke} from "@tauri-apps/api/tauri";
 import GetSVG from "./GetSVG.tsx";
 import PasswordStrengthIndicator from "./PasswordStrengthIndicator.tsx";
 import {message} from "@tauri-apps/api/dialog";
-import { appWindow } from '@tauri-apps/api/window'
+import {appWindow} from '@tauri-apps/api/window'
 
 /**
  * Settings page.
@@ -14,18 +14,19 @@ export default function Settings(): JSX.Element {
         <div class="flex flex-col m-10 items-center gap-3">
             <h1 class="font-bold text-3xl">Settings</h1>
             <SettingsForm>
-                <PasswordChangeForm />
+                <PasswordChangeForm/>
             </SettingsForm>
             <SettingsForm>
-                <CloudForm />
+                <CloudForm/>
             </SettingsForm>
         </div>
     )
 }
 
-function SettingsForm({children}: {children: JSX.Element}) {
+function SettingsForm({children}: { children: JSX.Element }) {
     return (
-        <div class="flex flex-col py-5 w-full justify-between items-center max-w-6xl rounded-md border border-[#E7E7E7] dark:border-[#3A3A3A] bg-[#F2F2F2] dark:bg-[#2B2B2B]">
+        <div
+            class="flex flex-col py-5 w-full justify-between items-center max-w-6xl rounded-md border border-[#E7E7E7] dark:border-[#3A3A3A] bg-[#F2F2F2] dark:bg-[#2B2B2B]">
             {children}
         </div>
     )
@@ -45,7 +46,7 @@ function PasswordChangeForm() {
             event.preventDefault();
             setLoading(true);
             try {
-                await invoke("change_password", { password: password(), confirm_password: confirmPassword() })
+                await invoke<void>("change_password", {password: password(), confirm_password: confirmPassword()})
                 await appWindow.emit("upload");
                 setError("");
                 setPassword("");
@@ -60,49 +61,59 @@ function PasswordChangeForm() {
         }}>
             <p class="text-[18px]">Change master password</p>
             <div class="relative w-full my-3">
-                <input placeholder="Enter new master password" type={visibility() ? 'text' : 'password'} class="pl-4 w-full h-7 rounded-xl pr-10" value={password()}
+                <input placeholder="Enter new master password" type={visibility() ? 'text' : 'password'}
+                       class="pl-4 w-full h-7 rounded-xl pr-10" value={password()}
                        onInput={async (event) => {
                            setPassword(event.target.value);
-                           setStrength(await invoke("password_strength", {password: event.target.value}) as number);
+                           setStrength(await invoke<number>("password_strength", {password: event.target.value}));
                        }}>
                 </input>
-                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer" onClick={() => setVisibility(!visibility())} title={(visibility() ? "Hide": "View")+" password"}>
-                    <GetSVG name={visibility() ? "eye-slash" : "eye"} class={`h-full p-1 ${visibility() ? 'pr-0.5' : ''}`} />
+                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+                     onClick={() => setVisibility(!visibility())}
+                     title={(visibility() ? "Hide" : "View") + " password"}>
+                    <GetSVG name={visibility() ? "eye-slash" : "eye"}
+                            class={`h-full p-1 ${visibility() ? 'pr-0.5' : ''}`}/>
                 </div>
             </div>
             <div class="relative w-full">
-                <input placeholder="Confirm new master password" type={confirmVisibility() ? 'text' : 'password'} class="pl-4 w-full h-7 rounded-xl pr-10" value={confirmPassword()}
+                <input placeholder="Confirm new master password" type={confirmVisibility() ? 'text' : 'password'}
+                       class="pl-4 w-full h-7 rounded-xl pr-10" value={confirmPassword()}
                        onInput={(event) => {
                            setConfirmPassword(event.target.value);
                        }}>
                 </input>
-                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer" onClick={() => setConfirmVisibility(!confirmVisibility())} title={(confirmVisibility() ? "Hide": "View")+" password"}>
-                    <GetSVG name={confirmVisibility() ? "eye-slash" : "eye"} class={`h-full p-1 ${confirmVisibility() ? 'pr-0.5' : ''}`} />
+                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+                     onClick={() => setConfirmVisibility(!confirmVisibility())}
+                     title={(confirmVisibility() ? "Hide" : "View") + " password"}>
+                    <GetSVG name={confirmVisibility() ? "eye-slash" : "eye"}
+                            class={`h-full p-1 ${confirmVisibility() ? 'pr-0.5' : ''}`}/>
                 </div>
             </div>
-            <Show when={strength() !==0}>
+            <Show when={strength() !== 0}>
                 <div class="mt-1 w-[95%] mx-auto">
-                    <PasswordStrengthIndicator strength={strength} />
+                    <PasswordStrengthIndicator strength={strength}/>
                 </div>
             </Show>
             <p class="text-[14px] text-[#EB5545]">{error()}</p>
-            <button type="submit" class="mt-2 flex flex-row items-center justify-center gap-2 h-7 w-32 rounded-xl mx-auto bg-[#3578F7] hover:bg-[#2F6AE1] text-[#E9E9E9]" title="Enter" disabled={loading()}>
+            <button type="submit"
+                    class="mt-2 flex flex-row items-center justify-center gap-2 h-7 w-32 rounded-xl mx-auto bg-[#3578F7] hover:bg-[#2F6AE1] text-[#E9E9E9]"
+                    title="Enter" disabled={loading()}>
                 <span class="bg-inherit">Change</span>
-                <GetSVG name={loading() ? "spinner" : "pen"} class={`w-4 fill-[#E9E9E9] ${loading() ? 'animate-spin' : ''}`} />
+                <GetSVG name={loading() ? "spinner" : "pen"}
+                        class={`w-4 fill-[#E9E9E9] ${loading() ? 'animate-spin' : ''}`}/>
             </button>
         </form>
     )
 }
 
 function CloudForm() {
-    const [status,{refetch: refetchStatus}] = createResource(async () => {
+    const [status, {refetch: refetchStatus}] = createResource(async () => {
         try {
-            let result = await invoke<{address: string, username: string}>("cloud_data");
+            let result = await invoke<{ address: string, username: string }>("cloud_data");
             setAddress(result.address);
             setUsername(result.username);
             return true
-        }
-        catch (e) {
+        } catch (e) {
             return false
         }
     })
@@ -119,11 +130,14 @@ function CloudForm() {
             setLoading(true);
             try {
                 if (status()) {
-                    await invoke("disable_cloud");
+                    await invoke<void>("disable_cloud");
                     await message("Cloud disabled successfully!", {title: "Success", type: "info"})
-                }
-                else {
-                    await invoke("enable_cloud", { address: address(), username: username(), password: password() });
+                } else {
+                    await invoke<void>("enable_cloud", {
+                        address: address(),
+                        username: username(),
+                        password: password()
+                    });
                 }
 
                 setError("");
@@ -137,7 +151,8 @@ function CloudForm() {
             }
         }}>
             <p class="text-[18px] mb-3">Cloud</p>
-            <input placeholder="IP address with port number" type="text" class="pl-4 w-full h-7 rounded-xl pr-10" value={address()}
+            <input placeholder="IP address with port number" type="text" class="pl-4 w-full h-7 rounded-xl pr-10"
+                   value={address()}
                    onInput={async (event) => {
                        setAddress(event.target.value);
                    }}>
@@ -148,19 +163,26 @@ function CloudForm() {
                    }}>
             </input>
             <div class="relative w-full">
-                <input placeholder="Password" type={visibility() ? 'text' : 'password'} class="pl-4 w-full h-7 rounded-xl pr-10" value={password()}
+                <input placeholder="Password" type={visibility() ? 'text' : 'password'}
+                       class="pl-4 w-full h-7 rounded-xl pr-10" value={password()}
                        onInput={(event) => {
                            setPassword(event.target.value);
                        }}>
                 </input>
-                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer" onClick={() => setVisibility(!visibility())} title={(visibility() ? "Hide": "View")+" password"}>
-                    <GetSVG name={visibility() ? "eye-slash" : "eye"} class={`h-full p-1 ${visibility() ? 'pr-0.5' : ''}`} />
+                <div class="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+                     onClick={() => setVisibility(!visibility())}
+                     title={(visibility() ? "Hide" : "View") + " password"}>
+                    <GetSVG name={visibility() ? "eye-slash" : "eye"}
+                            class={`h-full p-1 ${visibility() ? 'pr-0.5' : ''}`}/>
                 </div>
             </div>
             <p class="text-[14px] text-[#EB5545]">{error()}</p>
-            <button type="submit" class="mt-2 flex flex-row items-center justify-center gap-2 h-7 w-40 rounded-xl mx-auto bg-[#3578F7] hover:bg-[#2F6AE1] text-[#E9E9E9]" title={!status() ? "Setup" : "Remove"} disabled={loading()}>
+            <button type="submit"
+                    class="mt-2 flex flex-row items-center justify-center gap-2 h-7 w-40 rounded-xl mx-auto bg-[#3578F7] hover:bg-[#2F6AE1] text-[#E9E9E9]"
+                    title={!status() ? "Setup" : "Remove"} disabled={loading()}>
                 <span class="bg-inherit">{!status() ? "Setup" : "Remove"}</span>
-                <GetSVG name={loading() ? "spinner" : (!status() ? "cloud" : "xmark")} class={`w-4 fill-[#E9E9E9] ${loading() ? 'animate-spin' : ''}`} />
+                <GetSVG name={loading() ? "spinner" : (!status() ? "cloud" : "xmark")}
+                        class={`w-4 fill-[#E9E9E9] ${loading() ? 'animate-spin' : ''}`}/>
             </button>
         </form>
     )
